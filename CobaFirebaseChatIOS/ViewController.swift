abe57ed91f0a42fdd0394658b9f65b9d4e294b65
pratamawijaya <set.mnemonix@gmail.com>
2017-03-30 12:02:23 +0700

    Initial Commit

---
+++ CobaFirebaseChatIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CobaFirebaseChatIOS
+//
+//  Created by Pratama Nur Wiaya on 3/30/17.
+//  Copyright © 2017 PratamaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
